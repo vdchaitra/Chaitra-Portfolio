c9c604b86ae8ec3c1af430f1850d6b72a9672185
chaitra <chaitravmd3@gmail.com>
2025-06-13 10:32:35 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <div style={{ width: '100vw', height: '100vh', margin: 0, padding: 0 }}>
+      <iframe
+        style={{
+          border: 'none',
+          width: '100%',
+          height: '100%'
+        }}
+        src="https://embed.figma.com/proto/PyR3bOHZh3cQS9V6MKWcEH/Portfolio?page-id=0%3A1&node-id=1-2&p=f&viewport=97%2C33%2C0.15&scaling=min-zoom&content-scaling=fixed&starting-point-node-id=1%3A2&embed-host=share"
+        allowFullScreen
+        title="Figma Portfolio Prototype"
+      ></iframe>
+    </div>
+  );
+}
+
+export default App;
